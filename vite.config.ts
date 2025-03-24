a878fc620404fa003dc15c3477ab543150cdaced
Steven Barquet <mlmjm123@gmail.com>
2025-03-24 06:45:45 -0600

    prompt de imagenes mejorado y configuración para PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,35 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Self GPT',
+        short_name: 'SelfGpt',
+        description: 'Your personal AI assistant, open source and FREE to use.',
+        theme_color: '#1e498f',
+        icons: [
+          {
+            src: '/src/assets/192-logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/src/assets/512-logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@node_modules': './node_modules',
